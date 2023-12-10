fddb1d537e357e4f3f11483c9c4c837b371690a2
kubelin <jongho8765@gmail.com>
2023-12-10 21:14:19 +0900

    Delete my-pet-manager directory

--- my-pet-manager/src/main/java/com/mypetmanager/global/infra/GeneralJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mypetmanager.global.infra;
-
-import com.mypetmanager.global.annotation.domain.RootDomain;
-
-public abstract class GeneralJpaRepository<A extends RootDomain> {
-
-}
--- my-pet-manager/src/main/java/com/mypetmanager/integration/repository/pet/PetJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mypetmanager.integration.repository.pet;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mypetmanager.integration.repository.pet.entity.Pet;
-
-public interface PetJpaRepository extends JpaRepository<Pet, Long>{
-
-}
